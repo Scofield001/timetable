19fbf134efc1dfe30b00443e9c2724544cf4efba
Scofield001 <scofield-001@mail.ru>
2019-02-15 02:10:21 +0300

    Задачи к плагинам

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const gulp = require( 'gulp' ),
+    autoprefixer = require ('gulp-autoprefixer'),
+    babel = require('gulp-babel'),
+    concat = require( 'gulp-concat' ),
+    cssMin = require('gulp-csso'),
+    htmlMin = require('gulp-htmlmin'),
+    rename = require('gulp-rename'),
+    sass = require( 'gulp-sass' ),
+    uglify = require('gulp-uglify'),
+
+    paths = {
+        sass: [
+            'lib/style/*',
+            'common.blocks/**/*.scss',
+        ],
+        js: [
+            'js/render.js',
+            'js/filter.js',
+            'js/buttons.js',
+            '!js/build.min.js',
+        ],
+    };
+
+gulp.task('babel', function () {
+    gulp.src( paths.js)
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe( concat( 'build.min.js' ))
+        .pipe(gulp.dest('dist/js/'))
+        .pipe(gulp.dest('js/'));
+    gulp.src('js/getJSON.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('sass', function () {
+    gulp.src( paths.sass )
+        .pipe( concat( 'style.scss' ))
+        .pipe( sass())
+        .pipe(autoprefixer({
+            browsers: ['last 4 version'],
+            cascade: false
+        }))
+        .pipe(gulp.dest( 'style/' ))
+        .pipe(cssMin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/style/'))
+        .pipe(gulp.dest('style/'));
+
+});
+
+gulp.task('html', function () {
+    gulp.src('*.html')
+        .pipe(htmlMin({removeComments: true, collapseWhitespace: true}))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', function () {
+    gulp.src('lib/*.js')
+        .pipe(gulp.dest('dist/lib'));
+    gulp.src('responses/*.json')
+        .pipe(gulp.dest('dist/responses'))
+});
+
+gulp.task('default', gulp.series(gulp.parallel('html', 'sass', 'babel')));
