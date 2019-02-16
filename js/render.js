b1b569bae34a0b03ca0c1ee690f3ae58a446745a
Scofield001 <scofield-001@mail.ru>
2019-02-16 19:17:09 +0300

    Исправлена логика рендера. Убрал лишний код

--- js/render.js
+++ js/render.js
@@ -1,62 +1,68 @@
 'use strict';
 
+let table = $('.table');
+
 /**
  * Показывает все рейсы
  * @param schedule {array} принимает список рейсов (вылет / прилет)
  */
 function render(schedule) {
-    let tableFilter = $('.table');
+
     //Строки
     for (let i in schedule) {
-        let tr = $('<tr>', {class: 'table__row'}),
+        // if (!schedule[i].is_fuzzy) continue;
+        renderTable(schedule, i);
+    }
+}
 
-            info = {
-                flightNum: schedule[i].thread.number,       //Номер рейса
-                direction: schedule[i].departure,           //Время вылета
-                route: schedule[i].thread.title,            //Маршрут
-                company: schedule[i].thread.carrier.title,  //Имя перевозчика
-                time: schedule[i].departure,                //Время прилета(условно)
-                delay: schedule[i].is_fuzzy,                //Опоздание
-            };
-        tableFilter.append(tr);
+function renderTable(schedule, i) {
+    let tr = $('<tr>', {class: 'table__row'}),
+        info = {
+            flightNum: schedule[i].thread.number,       //Номер рейса
+            direction: schedule[i].departure,           //Время вылета
+            route: schedule[i].thread.title,            //Маршрут
+            company: schedule[i].thread.carrier.title,  //Имя перевозчика
+            time: schedule[i].departure,                //Время прилета(условно)
+            delay: schedule[i].is_fuzzy,                //Опоздание
+        };
+    table.append(tr);
 
-        //Ячейки
-        for (let j in info) {
-            let td = $('<td>', {class: 'table__col'});
+    //Ячейки
+    for (let j in info) {
+        let td = $('<td>', {class: 'table__col'});
 
-            // Т.к в объекте время находится в разных местах, нужно сделать проверку на пустое свойство
-            if (schedule[i].departure !== null) {
-                //Убираем лишние данные времени
-                info.time = schedule[i].departure.split('T')[0];
-            } else if (info.direction === null) {
-                info.direction = schedule[i].arrival;
-            } else if (info.time === null) {
-                info.time = schedule[i].arrival.split('T')[0];
-            }
-
-            //Добавляем модификатор маршрутам
-            if (info[j] === info.route) {
-                td.addClass('table__col_font');
-            }
-
-            //Проверка на задержку рейса
-            /*TODO: Из полученных данных, все рейсы не задержаны,
-                    поэтому протестировал вручную на локальном json изменив is_fuzzy на true.*/
-            //Добавляем модификатор примечаниям
-            switch (info[j]) {
-                case true:
-                    td.addClass('table__col_color').text('Задержан');
-                    break;
-                case false:
-                    td.text(`Ожидается в ${info.direction}`);
-                    break;
-                default:
-                    td.text(info[j])
-            }
-            //Меняем формат времени
-            info.direction = info.direction.split('T').pop().replace(/:\d+\+\d+:\d+/g, '');
-            tr.append(td);
+        // Т.к в объекте время находится в разных местах, нужно сделать проверку на пустое свойство
+        if (schedule[i].departure !== null) {
+            //Убираем лишние данные времени
+            info.time = schedule[i].departure.split('T')[0];
+        } else if (info.direction === null) {
+            info.direction = schedule[i].arrival;
+        } else if (info.time === null) {
+            info.time = schedule[i].arrival.split('T')[0];
         }
+
+        //Добавляем модификатор маршрутам
+        if (info[j] === info.route) {
+            td.addClass('table__col_font');
+        }
+
+        //Проверка на задержку рейса
+        /*TODO: Из полученных данных, все рейсы не задержаны,
+                поэтому протестировал вручную на локальном json изменив is_fuzzy на true.*/
+        //Добавляем модификатор примечаниям
+        switch (info[j]) {
+            case true:
+                td.addClass('table__col_color').text('Задержан');
+                break;
+            case false:
+                td.text(`Ожидается в ${info.direction}`);
+                break;
+            default:
+                td.text(info[j])
+        }
+        //Меняем формат времени
+        info.direction = info.direction.split('T').pop().replace(/:\d+\+\d+:\d+/g, '');
+        tr.append(td);
     }
 }
 
@@ -65,52 +71,11 @@ function render(schedule) {
  * @param schedule {array} принимает список рейсов
  */
 function renderDelay(schedule) {
-    let tableFilter = $('.table');
 
+    //Строки
     for (let i in schedule) {
-        //Убираем Незадержанные рейсы
         if (!schedule[i].is_fuzzy) continue;
-
-        let tr = $('<tr>', {class: 'table__row'}),
-            info = {
-                flightNum: schedule[i].thread.number,       //Номер рейса
-                direction: schedule[i].departure,           //Время вылета
-                route: schedule[i].thread.title,            //Маршрут
-                company: schedule[i].thread.carrier.title,  //Имя перевозчика
-                time: schedule[i].departure,                //Время прилета(условно)
-                delay: schedule[i].is_fuzzy,                //Опоздание
-            };
-        tableFilter.append(tr);
-
-        //TODO: Получаем только задержанные рейсы
-        for (let j in info) {
-            let td = $('<td>', {class: 'table__col'});
-
-            if (schedule[i].departure !== null) {
-                info.time = schedule[i].departure.split('T')[0];
-            } else if (info.direction === null) {
-                info.direction = schedule[i].arrival;
-            } else if (info.time === null) {
-                info.time = schedule[i].arrival.split('T')[0];
-            }
-
-            if (info[j] === info.route) {
-                td.addClass('table__col_font');
-            }
-
-            switch (info[j]) {
-                case true:
-                    td.addClass('table__col_color').text('Задержан');
-                    break;
-                case false:
-                    td.text(`Ожидается в ${info.direction}`);
-                    break;
-                default:
-                    td.text(info[j])
-            }
-            info.direction = info.direction.split('T').pop().replace(/:\d+\+\d+:\d+/g, '');
-            tr.append(td);
-        }
+        renderTable(schedule, i);
     }
 }
 
